1cc2a95d6ed1d60f8d7dd5754f1d03c3840a431d
Anatoly M <m@m.com>
2020-05-14 03:27:31 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,9 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long expected = 30;
+    long actual = service.calculate(amount, registered);
+    boolean pass = expected == actual;
+    System.out.println(pass);
   }
 }
-
